b194dd242cc7980256575d66a57f05ce9334f619
Luca Colagiorgio <l.colagiorgio@hotmail.com>
2020-02-11 18:56:34 +0100

    created backend & Api
    Backend & API created (mysql & asp.net)
    tests with postman successfull

---
+++ errorkb_backend/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace errorkb_backend.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ errorkb_backend/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ErrorkbDataAccess;
+
+namespace Errorkb_backend.Controllers
+{
+    public class UserController : ApiController
+    {
+        public IEnumerable<user> Get()
+        {
+            using (errorkbEntities1 entities = new errorkbEntities1())
+            {
+                return entities.users.ToList();
+            }
+        }
+
+        public user Get(string username)
+        {
+            using (errorkbEntities1 entities = new errorkbEntities1())
+            {
+                return entities.users.FirstOrDefault(e => e.username == username);
+            }
+        }
+
+    }
+}
