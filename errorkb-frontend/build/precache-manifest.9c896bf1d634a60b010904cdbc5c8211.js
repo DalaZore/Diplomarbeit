a6f32d481897bc3a38c79a2bbeedc67d488fc17e
Luca Colagiorgio <l.colagiorgio@hotmail.com>
2020-03-29 13:59:47 +0200

    build

---
+++ errorkb-frontend/build/precache-manifest.9c896bf1d634a60b010904cdbc5c8211.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d959a12e837d714581a433f196a731e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1be69c979d4345935b13",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "c04f67b0fdbb1724ce6e",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1be69c979d4345935b13",
+    "url": "/static/js/2.329f2535.chunk.js"
+  },
+  {
+    "revision": "5bcd3fd096ab86ff84e1348c0d76939e",
+    "url": "/static/js/2.329f2535.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c04f67b0fdbb1724ce6e",
+    "url": "/static/js/main.956462e4.chunk.js"
+  },
+  {
+    "revision": "9a0863e81c8b50eadd2d",
+    "url": "/static/js/runtime-main.35ba82a7.js"
+  }
+]);
